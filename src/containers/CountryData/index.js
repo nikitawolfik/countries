9936aefab53223d0854bf66fd5c44057768b3255
Nikita Neganov <nick.n@outlinestudio.co>
2019-11-14 22:18:31 +0500

    config

---
+++ src/containers/CountryData/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+import styles from './styles.module.scss';
+
+
+const CountryData = ({ countries }) => {
+  return (
+    <div className={styles.container}>
+      Hello
+    </div>
+  );
+};
+
+CountryData.propTypes = {
+  
+};
+
+export default CountryData;
---
+++ src/containers/index.js
@@ -0,0 +1,1 @@
+export { default as CountryData } from './CountryData';
